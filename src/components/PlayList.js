d48712d6235b7e02b07a211bf0db953b6ff54911
juleni <69912179+juleni@users.noreply.github.com>
2023-02-07 13:06:37 +0100

    Updated playlist

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -34,6 +34,302 @@ export const PLAYLIST = [
     genre: GENRES.AMBIENT,
   },
   {
+    title: "Horsehead Nebula",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/horseheadnebula.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/697.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Somehow",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/somehow.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/692.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Cascade to Heaven",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/cascade.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/689.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Coming Home To Andromeda",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/hometoandromeda.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/682.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "The Inspiration",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/inspiration.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/675.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Pure",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/pure.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/666.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Dreams To Remember",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/dreamsToRemember.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/657.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Cave Worlds",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/caveworlds.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/651.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Back To Earth",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/backtoearth.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/647.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Floating",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/floating.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/636.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "The Art Of Moving",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/theartofmoving.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/628.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Inter Stella Part 1-4",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/interstella.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/608.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Still Intense",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/stillintense.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/562.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Believing - Ambient Mix",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/believingambient.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/526.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Flowing Movements",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/flowingmovements.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/481.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Hang Drum Serenade",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/hangdrumserenade.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/476.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Rising Air",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/risingair.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/475.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Mikrokosmos",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/mikrikosmos.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/450.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "The Longest Journey",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/thelongestjourney.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/444.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Everlasting",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/everlasting.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/420.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Gamma Ray",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/gammaray.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/408.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Quantum Mechanics",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/quantummechanics.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/400.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Orbit",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/orbit.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/385.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Tranquility",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/tranquility.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/368.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Deep Breath",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/deepbreath.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/350.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Andromeda",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/andromeda.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/313.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Dream Sequence",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/dreamsequence.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/286.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "State Of Mind",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/stateofmind.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/266.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Destination Milky Way",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/destinationmilkyway.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/248.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Under Water",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/underwater.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/243.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Futurism",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/futurism.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/235.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Mermaid",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/mermaid.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/199.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Blue Planet Part I",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/blueplanet1.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/173.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Blue Planet Part III",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/blueplanetiii.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/178.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Something Out There",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/somethingoutthere.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/176.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Laser Beam",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/laserbeam.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/169.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
+    title: "Ethno Mystic",
+    artist: "Unknown Artist",
+    img_src:
+      "https://www.allesgemafrei.de/images/product_images/info_images/ethnomystic.png",
+    audio_src: "https://www.allesgemafrei.de/previews/Demo-Download/2.mp3",
+    genre: GENRES.AMBIENT,
+  },
+  {
     title: "Back In Time",
     artist: "Unknown Artist",
     img_src:
